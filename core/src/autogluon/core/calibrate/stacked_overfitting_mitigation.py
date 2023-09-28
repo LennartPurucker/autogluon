536fcf726306560a8669b6e7adbd8f88bd9ec532
Lennart Purucker <purucker@amazon.com>
2023-09-28 14:18:07 -0700

    final changes to clean oof before test

--- core/src/autogluon/core/calibrate/stacked_overfitting_mitigation.py
+++ core/src/autogluon/core/calibrate/stacked_overfitting_mitigation.py
@@ -10,18 +10,21 @@ from autogluon.common.features.feature_metadata import FeatureMetadata
 def clean_oof_predictions(X: pd.DataFrame, y: pd.Series, feature_metadata: FeatureMetadata, problem_type: str) -> Tuple[pd.DataFrame, pd.Series]:
     # FIXME: ignores sample weights so far, would need to add them here too.
 
-    stack_cols = feature_metadata.get_features(required_special_types=['stack'])
+    stack_cols = feature_metadata.get_features(required_special_types=["stack"])
     if not stack_cols:
         return X, y
     X = X.copy()
 
     if problem_type == BINARY:
-        # TODO: verify assumption that proba are always positive class proba
+        # TODO: verify assumption that proba are always positive class proba (think this is true)
         for f in stack_cols:
-            reg = IsotonicRegression(y_max=1, y_min=0, out_of_bounds='clip', increasing=True)
+            reg = IsotonicRegression(y_max=1, y_min=0, out_of_bounds="clip", increasing=True)
             X[f] = reg.fit_transform(X[f], y)
 
     elif problem_type == MULTICLASS:
+        raise NotImplementedError(
+            f"{MULTICLASS} not yet supported!"
+        )  # multiclass requires entirely different implementation than the one working below to function well (future work)
         # FIXME: get this from outer scope somehow (could not find it)
         classes_in_X = set([f.rsplit("_")[-1] for f in stack_cols])
         base_models = set([f.rsplit("_", 1)[0] for f in stack_cols])
@@ -31,7 +34,9 @@ def clean_oof_predictions(X: pd.DataFrame, y: pd.Series, feature_metadata: Featu
         # assert set(np.unique(y.astype(str))) != classes_in_X)  # does not necessarily need to hold true for this to work
         class_overflow = classes_in_X - set(np.unique(y.astype(str)))  # TODO: verify required assumption
         if class_overflow:  # needs to hold
-            raise ValueError(f"The OOF predictions contain classes that are not in the label! The follow missing classes are missing in the label: {class_overflow}")
+            raise ValueError(
+                f"The OOF predictions contain classes that are not in the label! The follow missing classes are missing in the label: {class_overflow}"
+            )
 
         for bm, bm_stack_cols in bm_proba_map.items():
             c_ordered = np.unique(list(classes_in_X))
@@ -45,7 +50,7 @@ def clean_oof_predictions(X: pd.DataFrame, y: pd.Series, feature_metadata: Featu
                 y_ovr[~pos_class_mask] = 0
                 y_ovr = y_ovr.astype(int)
 
-                reg = IsotonicRegression(y_max=1, y_min=0, out_of_bounds='clip', increasing=True)
+                reg = IsotonicRegression(y_max=1, y_min=0, out_of_bounds="clip", increasing=True)
                 X[current_f] = reg.fit_transform(X[current_f], y_ovr)
 
             # Normalize the probabilities for multiclass following sklearn's implementation
@@ -57,7 +62,7 @@ def clean_oof_predictions(X: pd.DataFrame, y: pd.Series, feature_metadata: Featu
             X[f_ordered] = full_proba
         # TODO: add this during predict time of model: refactor everything
     elif problem_type == REGRESSION:
-        raise NotImplementedError()
+        raise NotImplementedError(f"{REGRESSION} not yet supported!")
     else:
         raise ValueError(f"Unsupported Problem Type for cleaning oof predictions! Got: {problem_type}")
 
@@ -74,4 +79,3 @@ def clean_oof_predictions(X: pd.DataFrame, y: pd.Series, feature_metadata: Featu
 # for class_idx, this_pred in zip(pos_class_indices, predictions.T):
 #     calibrator = IsotonicRegression(out_of_bounds="clip")
 #     X[f] = calibrator.fit_transform(this_pred, Y[:, class_idx], sample_weight)
-
